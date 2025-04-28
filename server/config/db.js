aab5a30f06982835c1720749d8df621e33f49c19
jadaily <33360651+jadaily@users.noreply.github.com>
2025-04-27 21:29:44 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Create a connection pool to the MySQL database
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'jadendaily',
+  user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'library_management',
   waitForConnections: true,
